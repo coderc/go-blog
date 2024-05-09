f12afbb566365c7cc4301f9c88050fda14c02457
coderc <shao.c.coding@gmail.com>
2024-05-09 23:34:27 +0800

    web env Fix

--- web/src/config/backendUrl.js
+++ web/src/config/backendUrl.js
@@ -1,5 +1,7 @@
-const backendUrl = process.env.BACK_ENT_URL
+const backendUrl = process.env.VUE_APP_BACK_ENT_URL
 
+console.log("env", process.env)
+console.log("backendUrl", backendUrl)
 export {
     backendUrl
 }
