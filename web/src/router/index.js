b3d42a64369c9f6ef1f67ae57169ad8c9a183140
coderc <shao.c.coding@gmail.com>
2024-05-10 22:14:19 +0800

    Modify vue-router history role

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import BlogView from "@/views/BlogView.vue";
 import LinkView from "@/views/LinkView.vue";
@@ -36,7 +36,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
