c8c714a75c017ee371393a2a5cf174d808a7a3cf
coderc <shao.c.coding@gmail.com>
2024-05-20 00:22:05 +0800

    Update App.vue

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,6 +2,10 @@
   <NavBar />
   <router-view/>
   <PingBackend/>
+  <div class="center" style="text-align: center">
+    <!-- ICP备案号链接 -->
+    <p>ICP备案号：<a href="http://www.beian.miit.gov.cn/" target="_blank">ICP备案号链接</a></p>
+  </div>
 </template>
 
 <script>
